4aa36d9e1e4042fe0863ada342dcadd276b4b792
PJTSearch <pjtsignups@gmail.com>
2022-04-07 16:43:34 -0700

    chore: Add license

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-use std::{fs, path::PathBuf, str::FromStr, sync::Mutex};
-use hotwatch::{Event, Hotwatch};
-use protobuf::Message;
-use structopt::StructOpt;
-use serde_derive::Deserialize;
-use OpenControllerLib::*;
-use once_cell::sync::Lazy;
-use tide::{Request, log::LevelFilter};
-use log::info;
+/*
+Copyright (C) 2022 PJTSearch
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use anyhow::{Context, Result};
+use hotwatch::{Event, Hotwatch};
+use log::info;
+use once_cell::sync::Lazy;
+use protobuf::Message;
+use serde_derive::Deserialize;
+use std::{fs, path::PathBuf, str::FromStr, sync::Mutex};
+use structopt::StructOpt;
+use tide::{log::LevelFilter, Request};
+use OpenControllerLib::*;
 
 mod OpenControllerLib;
 
